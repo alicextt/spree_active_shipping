64ed8677d42a3a6d91448118cd077f3b7520a1d2
Gregory Chernoff <gchernoff@godaddy.com>
2014-09-10 19:59:04 -0700

    Disabling the majority of the extra validation as it is very US specific as not all countries have state or zipcodes. It also was affecting account creations as these additional fields are not yet available.

--- app/models/spree/stock_location_decorator.rb
+++ app/models/spree/stock_location_decorator.rb
@@ -1,10 +1,13 @@
 Spree::StockLocation.class_eval do
-  validates_presence_of :address1, :city, :zipcode, :country_id
-  validate :state_id_or_state_name_is_present
-
-  def state_id_or_state_name_is_present
-    if state_id.nil? && state_name.nil?
-        errors.add(:state_name, "can't be blank")
-    end
-  end
+  validates_presence_of :country_id
+  # Disabling as the following validation is very US specific as not all countries have state or zipcodes
+  # also at time of account creation we do not have the other address information.
+  # validates_presence_of :address1, :city, :zipcode
+  # validate :state_id_or_state_name_is_present
+  #
+  # def state_id_or_state_name_is_present
+  #   if state_id.nil? && state_name.nil?
+  #       errors.add(:state_name, "can't be blank")
+  #   end
+  # end
 end
