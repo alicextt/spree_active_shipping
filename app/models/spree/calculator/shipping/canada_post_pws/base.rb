ca10f1d06bd86339a65f4d699c40c0a0550bb163
Marcelo B. Alexandre <marcelobalexandre@gmail.com>
2018-02-08 17:16:11 -0200

    NEMO-7585 - Add contract_id parameter to CanadaPostPws
    https://jira.godaddy.com/browse/NEMO-7585

--- app/models/spree/calculator/shipping/canada_post_pws/base.rb
+++ app/models/spree/calculator/shipping/canada_post_pws/base.rb
@@ -8,6 +8,7 @@ module Spree
           canada_post_options = {
             endpoint: NemoSpreeCore.configuration.canada_post[:endpoint],
             customer_number: Spree::ActiveShipping::Config[:canada_post_customer_number],
+            contract_id: Spree::ActiveShipping::Config[:canada_post_contract_id],
             api_key: Spree::ActiveShipping::Config[:canada_post_api_key],
             secret: Spree::ActiveShipping::Config[:canada_post_secret],
             language: I18n.locale
--- lib/spree/active_shipping/canada_post_pws_override.rb
+++ lib/spree/active_shipping/canada_post_pws_override.rb
@@ -2,14 +2,28 @@ module Spree
   module ActiveShipping
     module CanadaPostPWSOverride
       def self.included(base)
+        def initialize(options = {})
+          @contract_id = options[:contract_id]
+
+          super(options)
+        end
+
         base.class_eval do
           cattr_reader :name
+
+          attr_accessor :contract_id
 
           def self.default_location
             ActiveMerchant::Shipping::Location.new(
               country: 'CA',
               zip: 'K2B8J6'
             )
+          end
+
+          def contract_id_node(options)
+            return unless options[:contract_id] || contract_id.present?
+
+            XmlNode.new('contract-id', options[:contract_id] || contract_id)
           end
         end
       end
